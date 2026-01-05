2f228fc11879c7edf99197cc97d5cc1334ecb846
Crim <jaydason15@gmail.com>
2026-01-05 16:27:22 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!-- Side Navigation -->
+<link rel="stylesheet" href="/my-website/assets/styles.css"> <!-- Learned that adding '/' in front of the path makes it absolute -->
+<div class="sidebar">
+    <a href="/my-website/index.php">Home</a><br><br>
+    <a href="/my-website/pages/budget.php">Budget</a><br><br>
+    <a href="/my-website/pages/income.php">Income</a><br><br>
+    <a href="/my-website/pages/transaction.php">Transaction</a><br><br>
+    <a href="/my-website/pages/history.php">History</a>
+ </div>
